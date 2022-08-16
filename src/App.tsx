03456efd56b3dfc567f8d75e328f7803ff08b2d4
biamesquitap <beatrizmesquitta@yahoo.com>
2022-08-16 14:36:31 -0300

    2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import { BrowserRouter } from 'react-router-dom'
-import { defaultTheme } from ".styles/themes/default";
+import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 import { Router } from "./Router";
 
