86e1387abead1751eb956f8640b4256f9fbfff23
biamesquitap <beatrizmesquitta@yahoo.com>
2022-08-29 17:10:26 -0300

    seg

--- src/pages/Home/Components/NewCycleForm/index.tsx
+++ src/pages/Home/Components/NewCycleForm/index.tsx
@@ -1,6 +1,24 @@
 import { FormContainer, TaskInput, MinutesAmountInput} from "./styles"
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+import { useForm } from "react-hook-form"
 
 export function NewCycleForm () {
+  const newCycleForValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    minutesAmount: zod.number().min(5).max(60)
+  })
+  
+  type NewCycleFormData = zod.infer<typeof newCycleForValidationSchema>
+  
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleForValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    }
+  })
+  
   return (
     <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,5 @@
 import { HandPalm, Play } from "phosphor-react";
 import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod'
-import * as zod from 'zod'
 import { useEffect, useState } from "react";
 import { differenceInSeconds } from 'date-fns'
 
@@ -12,15 +10,6 @@ import {
 } from "./styles";
 import { NewCycleForm } from "./Components/NewCycleForm";
 import { Countdown } from "./Components/Countdown";
-
-
-const newCycleForValidationSchema = zod.object({
-  task: zod.string().min(1, 'Informe a tarefa'),
-  minutesAmount: zod.number().min(5).max(60)
-})
-
-type NewCycleFormData = zod.infer<typeof newCycleForValidationSchema>
-
 interface Cycle {
   id: string
   task: string
@@ -33,44 +22,10 @@ interface Cycle {
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([])
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
-
-  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
-    resolver: zodResolver(newCycleForValidationSchema),
-    defaultValues: {
-      task: '',
-      minutesAmount: 0,
-    }
-  })
 
   const activeCycle = cycles.find((cycle) => cycle.id == activeCycleId)
 
-  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
 
-  useEffect(() => {
-    let interval: number
-
-    if (activeCycle) {
-      interval = setInterval(() => {
-        const secondsDifference = differenceInSeconds(new Date(), activeCycle.startDate)
-
-        if (secondsDifference >= totalSeconds){
-          setCycles ((state) =>
-           state.map(cycle => {
-            if (cycle.id == activeCycleId) {
-              return { ...cycle, finishedDate: new Date() }
-            } else {
-              return cycle
-            }
-          }),
-          )
-          setAmountSecondsPassed(totalSeconds)
-          clearInterval(interval)
-        } else {
-          setAmountSecondsPassed(secondsDifference)
-        }        
-      }, 1000)
-    }
     return () => {
       clearInterval(interval)
     }
