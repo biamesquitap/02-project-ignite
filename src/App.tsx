e79fb2e6e909069bab875316b36ff7ecb8186698
Beatriz Mesquita <beatrizmesquitta@yahoo.com>
2022-09-02 19:34:47 -0300

    context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { ThemeProvider } from "styled-components";
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from "react-router-dom";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 import { Router } from "./Router";
+import { CyclesContextProvider } from "./contexts/CyclesContext";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
